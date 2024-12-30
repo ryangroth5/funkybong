6e1563abac34462f9c05143b08e922c33a612d10
RG <msu@cpusoftware.com>
2024-12-30 08:52:57 -0500

    refactored. trying to get a start screen, global state and a player count.

--- main.gd
+++ main.gd
@@ -1,79 +1,44 @@
 extends Node
-var score
 
-# Track assigned controllers
-var assigned_controllers: Dictionary = {}
+var score: int = 0
 
-# Maximum number of players
-const MAX_PLAYERS: int = 4
+@onready var controller_manager: Node = $ControllerManager
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass ;
+	controller_manager.controller_connected.connect(_on_controller_connected)
 
-func game_over():
+func game_over() -> void:
 	$ScoreTimer.stop()
 	$MobTimer.stop()
 	$HUD.show_game_over()
 
-func new_game():
+func new_game() -> void:
 	score = 0
-	get_tree().call_group("PlayersGroup", "queue_free");
-	assigned_controllers={}
-	
+	get_tree().call_group("PlayersGroup", "queue_free")
+	controller_manager.reset_controllers()
 
 func _on_start_timer_timeout() -> void:
-	$GlobalSpawnTimer.start();
-	$ScoreTimer.start();
-
+	$GlobalSpawnTimer.start()
+	$ScoreTimer.start()
 
 func _on_score_timer_timeout() -> void:
-	score += 1;
+	score += 1
 	$HUD.update_score(score)
 
-
 func _on_hud_start_game() -> void:
-	new_game();
+	new_game()
 
 func _on_global_spawn_timer_timeout() -> void:
 	var spawners = get_tree().get_nodes_in_group("MobSpawnersGroup")
 	for spawner in spawners:
 		spawner.spawn()
 
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed:
-		if event.keycode == KEY_ESCAPE:
-			get_tree().quit()
-	for i in range(MAX_PLAYERS): # Assuming controllers "c0" to "c4"
-		var action_name = "c%d_start" % i
-		if Input.is_action_just_pressed(action_name):
-			handle_start_pressed(i)
-
-func handle_start_pressed(controller_index: int) -> void:
-	var controller_id = "c%d" % controller_index
-	
-	# Check if the controller is already assigned to a player
-	if controller_id in assigned_controllers:
-		print("Controller %s already assigned to a player." % controller_id)
-		return
-	
-	# Assign controller to a new player if there is room
-	if assigned_controllers.size() < MAX_PLAYERS:
-		assigned_controllers[controller_id] = true
-		spawn_player(controller_index)
-	else:
-		print("Max players reached. Cannot assign controller %s." % controller_id)
-
-func handle_select_pressed(controller_index: int) -> void:
-	var controller_id = "c%d" % controller_index
-	# Example: Print a message or handle other logic for the "select" button
-	print("Select button pressed on controller %s." % controller_id)
+func _on_controller_connected(controller_index: int) -> void:
+	spawn_player(controller_index)
 
 func spawn_player(controller_index: int) -> void:
 	print("Spawning player for controller %d" % controller_index)
-	var player_scene = preload("res://player.tscn")
+	var player_scene = preload("res://players/player.tscn")
 	var player_instance = player_scene.instantiate()
 	add_child(player_instance)
 	player_instance.set_controller(controller_index)
