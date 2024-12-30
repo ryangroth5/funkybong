e5e2762c76f6ff37ef731fb1e20991b2dd4736c6
RG <msu@cpusoftware.com>
2024-12-30 08:16:15 -0500

    adding controllers, state, and hud

---
+++ game_state.gd
@@ -0,0 +1,13 @@
+extends Node
+
+# Example game state variables
+var player_count = 1
+var score = 0
+var health = 100
+var level_name = "Level1"
+
+func reset_state():
+    player_count = 1
+    score = 0
+    health = 100
+    level_name = "Level1"
