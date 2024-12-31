352c81f35fbaf55a700f53823a96035140a4c4cd
RG <msu@cpusoftware.com>
2024-12-31 16:01:13 -0500

    animations are back.

--- objects/spawner/spawner.gd
+++ objects/spawner/spawner.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	add_to_group("MobSpawnersGroup")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
